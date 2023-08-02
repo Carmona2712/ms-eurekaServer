4f0e13e92e4d6feec34a4a4f68ba92f5c88c8316
Ricardo Carmona <carmonaricardo9511@gmail.com>
2023-08-02 15:39:35 -0500

    Commit inicial

---
+++ src/test/java/com/raca/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raca.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
